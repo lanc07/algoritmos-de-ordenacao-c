f1a4fe712978251e15246b37419c9200b01ba7a9
Eduardoui <eduardobacelar2007@gmail.com>
2025-08-22 19:03:44 -0300

    Acrescentado o resto do exercício 2

--- exercicio2.c
+++ exercicio2.c
@@ -22,6 +22,19 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+// Função para encontrar a posição do maior valor
+int posicaoM(int arr[], int n) {
+    int maior = arr[0];
+    int pos = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maior) {
+            maior = arr[i];
+            pos = i;
+        }
+    }
+    return pos + 1;
+}
+
 int main() {
     int pontos[12] = {10, 22, 9, 15, 20, 30, 25, 18, 12, 5, 8, 14};
     int n = 12;
@@ -35,6 +48,14 @@ int main() {
     bubbleSort(copia, n);
     printf("\nBubble Sort (decrescente): ");
     exibe(copia, n);
-    printf("Maior pontuador ficou na posicao: 1\n");
+    printf("Maior pontuador ficou na posicao: %d\n");
+    printf("Lista original: ");
+    for (int i = 0; i < 12; i++) {
+        printf("%d ", pontos[i]);
+    }
+    printf("\n");
 
+    printf("Maior pontuador ficou na posicao: %d\n", posicaoM(pontos, 12));
+
+    return 0;
 }
